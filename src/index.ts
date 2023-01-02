331c61e17e6d15ddeeb690d57e84b549883fb3a4
divine <drnkwati@yahoo.com>
2023-01-03 02:30:02 +0400

    Initial commit

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,7 @@ import greeter from './core/greeter.js';
 (function (root) {
 	const node = document.createElement('div');
 	node.textContent = greeter('Jane Doe');
+	node.classList.add('card console');
+
 	document.body.appendChild(node);
 })(this);

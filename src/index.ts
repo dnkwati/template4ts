c3c1d59ad347beb0bb1b7be8608a997460d4ee0c
Nkwati Divine <drnkwati+github@gmail.com>
2023-01-19 12:01:29 +0400

    Initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,9 @@
 // export * from './core/greeter.js';
 import greeter from './core/greeter.js';
-// old school JS
-(function (root) {
-	const node = document.createElement('div');
-	node.textContent = greeter('Jane Doe');
-	node.classList.add('card');
-	node.classList.add('console');
 
-	document.body.appendChild(node);
-})(this);
+const node = document.createElement('div');
+node.textContent = greeter('Jane Doe');
+node.classList.add('card');
+node.classList.add('console');
+
+document.body.appendChild(node);
